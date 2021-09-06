c2b2255606c6bec66f23a4787adb492f6eb1b3a9
nicatnabiyev <56915973+nicatnabiyev@users.noreply.github.com>
2021-09-06 22:34:13 +0400

    Initial commit

---
+++ app/src/main/java/com/nicatnabiyev88/workmanagerdemo/UploadWorker.kt
@@ -0,0 +1,37 @@
+package com.nicatnabiyev88.workmanagerdemo
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
+
+class UploadWorker(context: Context,params: WorkerParameters) : Worker(context,params) {
+
+    companion object{
+        const val KEY_WORKER = "key_worker"
+    }
+
+    override fun doWork(): Result {
+        try {
+            val count = inputData.getInt(MainActivity.KEY_COUNT_VALUE, 0)
+            for (i in 0 until count) {
+                Log.i("myTag", "Loading... : $i")
+            }
+
+            val time = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+            val currentData = time.format(Date())
+
+            val outPutData = Data.Builder()
+                .putString(KEY_WORKER,currentData)
+                .build()
+
+            return Result.success(outPutData)
+        } catch (e:Exception){
+            return Result.failure()
+        }
+    }
+
+}
